0933e330b45a58d9ef167a3f8d48d18a3e8e2c00
Vikrant Jha <129522178+vikj-67@users.noreply.github.com>
2023-04-01 23:03:30 +0530

    Update com1.sh

--- mini_projects/cron_job_01/com1.sh
+++ mini_projects/cron_job_01/com1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+# creating files
 for((i=1;i<=10;i++))
 do
 	touch /home/dbda/viki/source/file$i.txt
 done
 
-for((i=1;i<=5;i++))
-do
-	touch /home/dbda/viki/source/pic$i.jpg
-done
+# another way to create multiple files
+touch /home/dbda/viki/source/pic{1..5}.jpg
