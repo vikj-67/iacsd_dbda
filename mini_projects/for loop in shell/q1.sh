a6660784f9df868a7478e9801a70fffc64ac4d39
Vikrant Jha <129522178+vikj-67@users.noreply.github.com>
2023-04-02 14:39:37 +0530

    Delete mini_projects/for loop in shell directory

--- mini_projects/for loop in shell/q1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ((i=1;i<=10;i++))
-do
-	echo -n "$i "
-done
-echo ""
--- mini_projects/for loop in shell/q6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter a number: " num
-
-for((i=1;i<=10;i++))
-do
-    	echo "$num x $i = $(expr $num \* $i)"
-done
-
